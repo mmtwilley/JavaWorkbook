17b2a4f33f632089f8700cd5cbdf539c57ca7c86
Marvin Twilley <mtwilley@fb.com>
2023-01-26 17:46:58 -0600

    my first commt

---
+++ Java-Bootcamp-Resources-main/Module1/2. Variables/Workbook 2.3/AppleStore.java
@@ -0,0 +1,35 @@
+public class AppleStore {
+    public static void main(String[] args) {
+        int numOfApples = 0;
+        int numOfCustomers = 0;
+        double profit = 0;
+
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
+
+        System.out.println("You picked 500 apples from an apple orchard");
+        numOfApples += 500;
+
+        System.out.println("Time for business! You're selling each apple for 40 cents");
+        profit += .4
+        System.out.println("One customer walked in. He bought 4 apples!");
+        numOfCustomers += 1;
+        numOfApples -= 4;
+        profit *= 4;
+        System.out.println("Another customer walked in. He bought 20 apples!");
+        numOfCustomers += 1;
+        numOfApples -= 20;
+        profit = profit + (20 * .4)
+        System.out.println("Another customer walked in. She bought 200 apples!");
+        numOfCustomers += 1;
+        numOfApples -= 200;
+        profit = profit + (200 * .4)
+
+        System.out.println("Wow! So far, you made: $%d%n",profit);
+        System.out.println("%s + customers love your apples.%n", numOfCustomers);
+        System.out.println("You have %d apples left.%n", numOfApples);
+
+        // Compare your result to what's on Learn the Part.
+
+    }
+
+}
