ad6de1e7a3bb0ffce64baa20de43383d86749ff0
Marvin Twilley <marvinmtwilley@gmail.com>
2023-02-24 01:22:19 -0600

    Completed the Java file.

--- Workbook_Answers/BooleansAndConditionals/Dealership/Dealership.java
+++ Workbook_Answers/BooleansAndConditionals/Dealership/Dealership.java
@@ -13,18 +13,46 @@ public class Dealership {
 
         String option = scan.nextLine();
 
+        int budget;
+
         switch(option){
             case "a": 
-               System.out.println("you chose option " + option);
+               System.out.println("What is your budget?");
+               budget = scan.nextInt();
+               
+               if(budget >= 10000){
+                    System.out.println("Great! A Nissan Altima is available");
+                    System.out.println("\n Do you have Insurance? Write 'yes' or 'no'?");
+                    scan.nextLine();
+                    String insurance  = scan.nextLine();
+                    System.out.println("\n Do you have Insurance? Write 'yes' or 'no'?");
+                    String license = scan.nextLine();
+                    System.out.println("\n What is your credit score?");
+                    int creditScore = scan.nextInt();
+                    if(insurance.equals("yes") && license.equals("yes") && creditScore > 600){
+                        System.out.println("Sold! Pleasure doing business with you!");
+                    }else{
+                        System.out.println("We're sorry. You are not eligible.");
+                    }
+            } else {
+                System.out.println(" We don't sell cars under $10,000. Sorry!");
+            }
                break;
             case "b":
-               System.out.println("you chose option " + option);
+               System.out.println("\n What is your car valued at?");
+               int value = scan.nextInt();
+               System.out.println("\n What is your selling price?");
+               int price = scan.nextInt();
+
+                if(value > price && price < 30000){
+                    System.out.println("\n We will buy your car. Pleasure doing business with you!");  
+                } else {
+                    System.out.println("\n Sorry, we're not interested.");
+                }
                break;
             default:
              System.out.println("invalid option");
-
         }
-
 
         scan.close();
     }
