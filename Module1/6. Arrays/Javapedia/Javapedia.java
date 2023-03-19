5595561eabcb5e5908a5a666f63bf1059eaa0fec
Marvin Twilley <marvinmtwilley@gmail.com>
2023-03-18 23:54:38 -0500

    Completed section 6

--- Module1/6. Arrays/Javapedia/Javapedia.java
+++ Module1/6. Arrays/Javapedia/Javapedia.java
@@ -7,54 +7,54 @@ public class Javapedia {
         System.out.println("How many historical figures will you register?");
         //Task 1 – Ask the user: how many historical figures will you register?
         //       – Store the value.
-        
+        int figures = scan.nextInt();
         //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-         
+        String[][] database = new String[figures][3];
         //Watch out for the nextLine() pitfall. 
-        /* Task 3 
-        for (____) {
+        scan.nextLine();
+       
+        for (int i = 0; i < database.length; i++) {
 
             System.out.println("\n\tFigure " + (i+1)); 
-
             System.out.print("\t - Name: ");
-            pick up and store figure's name.   
-
+            database[i][0] = scan.nextLine();             
             System.out.print("\t - Date of birth: ");
-            pick up and store figure's birthday.
-
+            database[i][1] = scan.nextLine();
             System.out.print("\t - Occupation: ");
-            pick up and store figure's occupation. 
-
+            database[i][2] = scan.nextLine();
             System.out.print("\n");
-
         }
         
-        */
+       
+
         System.out.println("These are the values you stored:"); 
         //Task 4: call print2DArray. 
 
-        System.out.print("\nWho do you want information on? ");  
-        
-        /*Task 5: Let the user search the database by name. 
-            If there's a match:
-              print(    tab of space    Name: <name>)
-              print(    tab of space    Date of birth: <date of birth>)
-              print(    tab of space    Occupation: <occupation>)
+        print2DArray(database);
 
-        */        
+        System.out.print("\nWho do you want information on? ");  
+
+        String figure = scan.nextLine();
+        search(figure, database);
 
         scan.close();
     }
 
-    /**
-     * Function name: print2DArray
-     * @param array (String[][])
-     * 
-     * Inside the function
-     *  1. print the database
-     *     • a tab of space precedes each row.
-     *     • each value in database has one space from the other value. 
-     *     • print a new line.
-     */
+
+    public static void print2DArray(String[][] database){
+        for(int i = 0; i < database.length; i++){
+            System.out.println("\t" + database[i][0] + " " + database[i][1] + " " + database[i][2]);
+        }
+    }
+
+    public static void search(String figure,String[][] database){
+        for(int i = 0; i < database.length; i++){
+            if(database[i][0].equals(figure)){
+                System.out.println("\t Name: " + database[i][0]);
+                System.out.println("\t Date of birth: " + database[i][1]);
+                System.out.println("\t Occupation: " + database[i][2]);
+            }
+        }
+    }
 
 }
