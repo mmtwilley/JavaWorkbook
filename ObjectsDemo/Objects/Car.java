6743543bfeeabbadcfecaff928cab5362d5ef025
mmtwilley <50936642+mmtwilley@users.noreply.github.com>
2023-03-22 20:45:48 +0000

    Added some more information to section 7

--- ObjectsDemo/Objects/Car.java
+++ ObjectsDemo/Objects/Car.java
@@ -1,13 +1,68 @@
 public class Car{
-    String make; 
-    double price;
-    int year; 
-    String color;
+    private String make; 
+    private double price;
+    private int year; 
+    private String color;
+    private String[] parts;
 
-    public Car(String make, double price, int year,String color ){
+
+    public Car(String make, double price, int year,String color, String[] parts ){
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
+        this.parts = parts;
     }
+
+    public Car(Car source){
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+        this.parts = source.parts;
+    }
+
+    public String getMake(){
+        return this.make;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+    public int getYear(){
+        return this.year;
+    }
+    public String getColor(){
+        return this.color;
+    }
+
+   public String[] getParts(){
+    return this.parts;
+   }
+
+    public void setMake(String make){
+        this.make = make;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setParts(String[] parts){
+        this.parts = parts;
+    }
+
+
+    public void drive(){
+        System.out.println(" You brought the beautiful " + this.year + " " + this.color + " " + " for "  + this.price + ".");
+        System.out.println("Please, drive to the nearest exit.");
+    }
+
 }
--- ObjectsDemo/Objects/Main.java
+++ ObjectsDemo/Objects/Main.java
@@ -1,14 +1,17 @@
  public class Main {
 
     // objects things that contains fields or can perform tasks. 
+    // Mutable Object = objects with setters
+    //  -- avoid setting variables equal to each other 
+    //  -- Both variables will share a reference to the same object
     public static void main(String[] args){
-        Car nissan = new Car("Nissan",10000, 2019, "blue");
-        Car dodge = new Car("Dodge",12000,2019,"green");
+        String[] spareParts = new String[]{"Tires","Keys","Filter","Brake"};
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price + 
-        ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price + 
-        ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
-    
+        Car nissan = new Car("Nissan",10000, 2019, "blue",spareParts);
+        Car dodge = new Car("Dodge",12000,2019,"green",spareParts);
+
+        Car nissan2 = new Car(nissan);
+
+        nissan2.drive();
     }
 }
