fd56827f6d673b5db218ab2995070e9cda36403c
mmtwilley <50936642+mmtwilley@users.noreply.github.com>
2023-03-28 22:52:02 +0000

    This is some more work

--- ObjectsDemo/Objects/Car.java
+++ ObjectsDemo/Objects/Car.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Car{
     private String make; 
     private double price;
@@ -11,7 +13,7 @@ public class Car{
         this.price = price;
         this.year = year;
         this.color = color;
-        this.parts = parts;
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
     public Car(Car source){
@@ -19,7 +21,7 @@ public class Car{
         this.price = source.price;
         this.year = source.year;
         this.color = source.color;
-        this.parts = source.parts;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
     }
 
     public String getMake(){
@@ -37,7 +39,7 @@ public class Car{
     }
 
    public String[] getParts(){
-    return this.parts;
+    return Arrays.copyOf(this.parts,this.parts.length);
    }
 
     public void setMake(String make){
@@ -56,7 +58,7 @@ public class Car{
     }
 
     public void setParts(String[] parts){
-        this.parts = parts;
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
 
--- ObjectsDemo/Objects/Main.java
+++ ObjectsDemo/Objects/Main.java
@@ -5,12 +5,8 @@
     //  -- avoid setting variables equal to each other 
     //  -- Both variables will share a reference to the same object
     public static void main(String[] args){
-        String[] spareParts = new String[]{"Tires","Keys","Filter","Brake"};
-
-        Car nissan = new Car("Nissan",10000, 2019, "blue",spareParts);
-        Car dodge = new Car("Dodge",12000,2019,"green",spareParts);
-
-        Car nissan2 = new Car(nissan);
+        Car nissan = new Car("Nissan",10000, 2019, "blue",new String[]{"Tires","Keys","Brake"});
+        Car dodge = new Car("Dodge",12000,2019,"green",new String[]{"Tires","Keys","Brake"});
 
         nissan2.drive();
     }
