b8f549481086c7a72bfe23052a9986b2c4f3c607
mmtwilley <50936642+mmtwilley@users.noreply.github.com>
2023-03-31 15:14:07 +0000

    Completed Workbook 1-5

---
+++ Workbook_Answers/Objects/7.5_Workbook/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+      Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+
+        if(person.applyPassport()){
+            System.out.println("Congratulations " + person.getName() + ". Your passport was approved!");
+        } else {
+            System.out.println("We are sorry " + person.getName() + ". We cannot process your application.");
+        }
+
+    }
+}
