2ca5220916640e6f86b40b16a003e63517485da6
Marvin Twilley <marvinmtwilley@gmail.com>
2023-04-23 00:41:22 -0500

    Completed Some work

---
+++ Workbook_Answers/ListCollectionExtra/Book.java
@@ -0,0 +1,75 @@
+package Workbook_Answers.ListCollectionExtra;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private double rating;
+    private double price;
+
+
+    public Book(String title, String author, double rating, double price){
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    public Book(Book source){
+        this.title = source.title;
+        this.author = source.author;
+        this.rating = source.rating;
+        this.price = source.price;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public double getRating(){
+        return this.rating;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setRating(double rating){
+        this.rating = rating;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Book)) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && rating == book.rating && price == book.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, rating, price);
+    }
+
+}
