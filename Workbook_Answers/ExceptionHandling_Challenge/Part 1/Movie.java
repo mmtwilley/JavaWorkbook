b86c4cfd576d339408daf3d21c6bdf16705f7080
Marvin Twilley <50936642+mmtwilley@users.noreply.github.com>
2023-04-27 01:23:17 +0000

    Completed the Exception Handling ExceptionHandling

---
+++ Workbook_Answers/ExceptionHandling_Challenge/Part 1/Movie.java
@@ -0,0 +1,57 @@
+public class Movie {
+    
+    private String name;
+    private String format;
+    private double rating;
+
+    public Movie(String name, String format, double rating) {
+        setName(name);
+        setFormat(format);
+        setRating(rating);
+    }
+
+    public Movie(Movie source) {
+        this.name = source.name;
+        this.format = source.format;
+        this.rating = source.rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isBlank() || name == null){
+            throw new IllegalArgumentException("The name cannot be blank or null!"); 
+        }
+        this.name = name;
+    }
+    
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        if(format.isBlank() || format == null){
+            throw new IllegalArgumentException("The name cannot be blank or null!");
+        }
+        // TODO
+        this.format = format;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        if(rating < 0 || rating > 10){
+            throw new IllegalArgumentException("The number should be in the range of 1 to 10!")
+        }
+        this.rating = rating;
+    }
+
+    public String toString() {
+        return this.rating + "\t" + this.format + "\t\t" + this.name + "";
+    }    
+
+}
